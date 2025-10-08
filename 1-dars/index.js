2d7b58860a8473050ed6dad415cb2f8ba44b1782
shox2552-png <shox2552@gmail.com>
2025-10-08 20:19:35 +0500

    changed

---
+++ 1-dars/index.js
@@ -0,0 +1,25 @@
+console.log("Hi Github");
+
+// 1. git init
+
+// 2. git remote add origen https://github.com/shox2552-png/JavaScriot.git
+
+// 3.git add .
+
+// 4.git commit -m "comentariya"
+
+// 5. git config --global user.email "shox2552@gmail.com"
+
+// 6. git config --global user.name "shox2552-png"
+
+// 7.git commit -m "comentariya"
+
+// 8.git push origin master
+
+// =======================================
+
+//  1. git add .
+
+//  2. git commit -m "changed"
+
+// 3.gir push origin master
