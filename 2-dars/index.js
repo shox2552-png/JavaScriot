23c85acf39f6f6e51d9d9e5cf6795f61b4e7859d
shox2552-png <shox2552@gmail.com>
2025-10-15 17:59:22 +0500

    3-dars

---
+++ 2-dars/index.js
@@ -0,0 +1,60 @@
+
+
+// console.log("Shoxjahon 1")
+// console.log("Shoxjahon 2")
+// console.log("Shoxjahon 3")
+
+// var matn="fdjfnvjdnfvmmklvlmvsnvdmjlsv"
+
+// let matn="neowfejocbebmrpdbmdjbpoem"
+
+// const matn=mrjsvkrneigorneovlmkdnrvoepve
+
+// var, let, const
+
+// console.log(matn);
+
+// console.log(matn);
+
+// var name="Eshmat";
+
+// var name="Toshmat";
+
+// console.log(name);
+
+// ==================================
+
+// let phone-9983895799270
+
+// let phone-998937597208
+
+// console.log(phone);
+
+// =================================
+
+// const name = "Eshmat";
+
+// const name= "Toshmat";
+
+// name= "Toshmat";
+
+// console.log(name)
+
+//undefined mavjud emas
+
+// let user_name = "Eshmat" snace_case
+
+// =================================================
+
+// 1. JS nima?  : dasturlash tili, odamning miyasiday muxim
+
+// 2. Interpreter? :node dasturi dastur kodini brauzerga tarjima kilib beradi
+
+// 3.Single Thread? :bir vaxt ichida 2ta ishni kila olmaydi
+
+// 4. Veriables nima? : O'zgaruvchilar aniq bir isimga ega
+
+// 5. Variables - var, let, const - farqlari?  
+// :var.agar hato kilsangiz ogoxlanrirmaydi 
+// let. hato qilsangiz oghlantiradi
+// const.facat bitta kiymat berish mumrin,  hato qilsangiz oghlantiradi
---
+++ 3-dars/index.js
@@ -0,0 +1,116 @@
+// console.log("hi JavaScript!")
+// console.log("Hi CSS")
+// console.log("Hi Html")
+
+// var name = "Eshmat"
+
+// console.log(name);
+
+// var data = "2011"
+
+// console.log(data)
+
+// var name = "Toshmat"
+
+// console.log(name)
+
+// var data = "18.03"
+
+// console.log(data)
+
+// var name = "Ulug'bek"
+
+// console.log(name)
+
+// var data = "2014"
+
+// console.log(data)
+
+// var name = "Valiev"
+
+// console.log(name)
+
+// var data = "28.07"
+
+// console.log(data)
+
+// var name = "Diyorbek"
+
+// console.log(name)
+
+// var data = "2018.16.12"
+
+// console.log(data)
+
+// let son = "29048"
+
+// console.log(son)
+
+// let number = "93709475"
+
+// console.log(number)
+
+// let vaht = "16:00"
+
+// console.log(vaht)
+
+// let vremya = "17:16"
+
+// console.log(vremya)
+
+// let a = "Ziyo"
+
+// console.log(a)
+
+// let b = "Dali"
+
+// console.log(b)
+
+// let c = "Vali"
+
+// console.log(c)
+
+// let d = "Toxr"
+
+// console.log(d)
+
+// let e = "Eshmat"
+
+// console.log(e)
+
+// let f = "Toshmat"
+
+// console.log(f)
+
+// const ad = "index"
+
+// console.log(ad)
+
+// alert("Salom")
+
+// prompt("Ismingizni yozing!")
+
+// confirm("Siz 19 yoshdamisiz")
+
+// let yosh = prompt("Yilizni yozing")
+
+// console.log(2025 - yosh)
+
+let year1 = prompt(" O'zingizni Yilizni kiriting")
+let one_age = 2025 - year1
+
+let year2 = prompt("Otangizni yoshini kiriting")
+let two_age = 2025 - year2
+
+let year3 = prompt("Onangizni yilini kiriting")
+let three_age = 2025 - year3
+
+let year4 = prompt("Ukangizni yilini kiriting")
+let four_age =2025 - year4
+
+let year5 = prompt("Ukangizni yilini kiriting")
+let five_age = 2025 - year5
+
+console.log((two_age + one_age + three_age + four_age + five_age) / 5)
+
+// 
