86125af11f4cd4d0022664fe84c0cdd0f43daa30
shox2552-png <shox2552@gmail.com>
2025-10-27 16:54:12 +0500

    changet

---
+++ 7-dars/index.js
@@ -0,0 +1,75 @@
+// && - and - va
+
+// console.log(1 && true && undefined && false && 0);
+
+// console.log(true || (0 && true) || (null && undefined) || 1);
+
+// ========================================================
+
+// ! - Not - Inkor
+
+// console.log(!0);
+
+// ==================================================
+// Falsiy - 0, false, "", null, undefined, NaN
+
+// ?? - Nullish
+
+// Falsiy - null yoki undefined
+
+// console.log(undefined ?? null ?? false ?? null);
+
+// ===================================================
+
+// if / else if / else
+
+// let age = prompt("Yoshingizni kiriting")
+
+// if (age >= 18 && age <= 30){
+//     console.log("Ruxsat");
+// }
+// else{
+//     console.log("Mumkin emas");
+
+// }
+
+//     let age = prompt("Yoshingizni kiriting")
+
+//     if ( age >=3 && age <=6){
+//         console.log("Maktabgacha ta'lim da o'qiydi");
+//    }
+//    else if(age >=7 && age <= 18){
+//     console.log("Maktabda o'qiydi");
+//    }
+//    else if(age >=19 && age <=30){
+//     console.log("univerda o'qiydi");    
+//    }
+//    else if(age >=31 && age <=40){
+//     console.log("Ishlaydi");
+//    }
+
+//    else if(age >=41 && age <=100){
+//     console.log("Nafaqa oladi");
+//    }
+//    else{
+//     console.log("O'lgan");
+//    }
+
+// ==================================================
+
+// Homework
+
+
+let name = prompt("Masinangizni yozing")
+
+if (name === "damas" || name === "nexia" || name === "cobalt") {
+    console.log("GM");
+}
+
+else if (name === "tesla model 3" || name === "tesla model s" || name === "tesla model x" || name === "tesla mode y") {
+    console.log("TESLA");
+}
+
+else if (name === "bmw m5" || name === "bmw i4" ) {
+    console.log("BMW");
+}
---
+++ 8-dars/index.js
@@ -0,0 +1,27 @@
+// let n = 0;
+// if (n === 0) {
+// console.log("du son toq ham emas, juft ham emas");
+// }
+// else if (n % 2) {
+// console.log("bu son toq");
+// } else {
+// console.log("bu son juft");
+
+// }
+
+// let name = promot("Ism kiriting");
+
+// switch (name){
+//     case "Eshmat":
+//         console.log("Ha name Eshmatga teng");
+//         return;
+//     case "Toshmat":
+//         console.log("Ha name Toshmatga teng");
+//         return
+//     case "Alibek":
+//         console.log("Ha name Alibekga teng");
+//         return
+//     default:
+//         console.log("Bu name bizda mavjud emas");
+// }
+
