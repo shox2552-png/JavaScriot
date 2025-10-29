b5fca364e236537f2070cf084a88676375db509b
shox2552-png <shox2552@gmail.com>
2025-10-29 16:07:01 +0500

    changet

--- 8-dars/index.js
+++ 8-dars/index.js
@@ -1,4 +1,4 @@
-// let n = 0;
+// let n = 10;
 // if (n === 0) {
 // console.log("du son toq ham emas, juft ham emas");
 // }
@@ -25,3 +25,112 @@
 //         console.log("Bu name bizda mavjud emas");
 // }
 
+// let n = prompt("raqam kiriting");
+
+// switch (n%2){
+//     case 1:
+//         console.log("bu son toq");
+//     break;
+//         case 0:
+//         console.log("bu son juft");
+//     break;
+//     default:
+//         console.log("son xato kiritildi");
+// }
+
+// let age = prompt("yoshingizni yozing");
+
+// switch (age > 18) {
+//     case true:
+//         console.log("18dan katta");
+//         break;
+//     case false:
+//         console.log("18dan kichik");
+//         break;
+//     default:
+//         console.log("qiymat xato");
+// 
+// ==========================================
+
+// Ternary operator
+
+//  ?, :
+
+// let  n = 29;
+
+// n % 2 ? console.log("bu son toq") : console.log("bu son juft");
+
+// let a = 5;
+
+// let b = 10;
+
+// let c = 30;
+
+// let sun = (a+b+c%3)
+
+// switch(sun%2){
+//     case 1 : 
+//        console.log("bu son toq");
+//        break;
+
+//      case 0: 
+//      console.log("bu son juft");
+//      break;
+
+// }
+
+// ===============================
+
+// homework
+
+// let result;
+
+// if ( a + b < 4){
+//   result = 'below'
+// }else{
+//   result = 'over'
+//   console.log();
+// }
+
+// let message;
+
+// if ( login == 'employee'){
+//   message = 'helo'
+// }else if(
+//   login == 'director'){
+//     message = 'greetings' 
+// } else if(login == ''){
+//   message = 'no login'
+// }else{
+//   message = ''
+// }
+
+// switch(browser){
+// case'edge':
+// alert("you,ve got the edgel");
+// break;
+// 
+// case'chrome':
+// case'firefox':
+// case'safari':
+// case'opera':
+// alert('okay we suppor these browsers too ');
+// break;
+// 
+// default:
+// alert('we hope that this page looks ok!');
+// 
+// }
+
+// let a = +prompt('a?', '');
+
+// if (a == 0) {
+//   alert(0);
+// }
+// if (a == 1) {
+//   alert(1);
+// }
+
+// if (a == 2 || a == 3){
+//   alert('2,3');
+// }
