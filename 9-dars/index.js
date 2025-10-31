d711b7a4862056008984a6a7cffeceeaaac03bda
shox2552-png <shox2552@gmail.com>
2025-10-31 16:03:17 +0500

    changed

---
+++ 9-dars/index.js
@@ -0,0 +1,90 @@
+// let data = 21;
+
+// if (data % 2 === 1) console.log("Toq son");
+// else if (data % 2 === 0) console.log("Juft son");
+// else console.log("Siz notogri qiymat kiritingiz");
+
+// =====================================
+
+// data % 2 === 1 ? console.log("Toq Son") : console.log("Juft son");
+
+// data % 2 === 1
+//     ? console.log("Toq son")
+//     : data % 2 === 0
+//     ? console.log("Juft son")
+//     : console.log("Siz notogri qiymat kiritingiz");
+
+
+// for(start; condition; step);
+
+// for (let a = 100; a >=1; a--){
+//     console.log(a);
+
+// }
+
+// for (let i = 1; i <= 10; i++){
+//     if( i % 2 === 1){
+//         console.log("Bu son toq");
+//     }else{
+//         i % 2 === 0
+//          console.log("Bu son juft");
+//     }
+// }
+
+// continue
+
+// for (let i = 1; i <= 10; i++){
+//     if (i === 5 || i === 6) continue;
+//     console.log(i);
+// }
+
+// // break
+// for (let a = 1; 1; a++){
+//     if (a === 1000)break;
+//     console.log(a);
+// } 
+
+// let sum = 0
+
+// for (let i = 1; i <=10; i++){
+//     sum = sum + i;
+// }
+//     console.log(sum);
+
+// let sum_toq = 0
+// let sum_juft = 0
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2) {
+//         sum_toq = sum_toq + i
+//     } else {
+//         sum_juft = sum_juft + i
+//     }
+// }
+
+// console.log(sum_toq);
+// console.log(sum_juft);
+
+
+// =========================================
+// HOMEWORK
+
+// let i = 0
+
+// for (let i = 0; i < 5; i++) alert(i);{
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 5; ++i){
+//     console.log(i);
+// }
+    
+// let a = 99
+
+// for (let a = 99; a < 99; a++)prompt("Son kiriting");{
+//     if (100 > a)
+//         console.log("salom");
+//     else{
+//         a > console.log("mumkin emas");
+//     }
+// }
