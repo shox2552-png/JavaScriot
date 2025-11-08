1939430f79fa26e8600cf035ede5c65e8453c837
shox2552-png <shox2552@gmail.com>
2025-11-08 14:14:01 +0500

    changet

---
+++ 13-dars/index.js
@@ -0,0 +1,153 @@
+// let obj = {};
+
+// let obj2 = new Object();
+
+// let obj3 = Object()
+
+// let obj = {
+//     ism: "Shoxjahon",
+//     familiya: "G'aybullayev",
+//     yoshi: 17,
+// };
+
+// console.log(obj.ism); // static
+
+// let key = "familiya";
+// console.log(obj[key]); // dinamik
+
+// let obj1 = {
+// ism: "Eshmatbek"
+// };
+
+// let obj2 = Object.assign({}, obj1);
+// let obj2 = structuredClone(obj1)
+
+// obj2.ism = "Toshmatbek";
+
+// console.log(obj1.ism);
+
+// =========================================
+
+// Loop - for in
+
+// let obj = {
+//     ism: "Otabek",
+//     familiya:"Valiyev",
+//     yoshi: 14,
+//     manzil: "Sadacha MFY",
+// };
+
+// for (let key in obj){
+//     console.log(key, " = ", obj[key]);
+// }
+
+// let obj = {
+//     ism: "Otabek",
+//     familiya:"Valiyev",
+//     yoshi: 14,
+//     manzil: "Sadacha MFY",
+// };
+
+// for (let key in obj){
+//     console.log(key, " = ", obj[key]);
+// }
+
+// console.log("===============================================");
+
+// let a = Object.assign({}, obj)
+// a.ism = "Toshmat"
+// a.familiya = "Aliyev"
+// a.yoshi = 13
+// for (let w in a){
+//     console.log(w, " = ", a[w]);
+// }
+
+// console.log("==================================================");
+
+// let obj2 = {
+//     ism: "Ulug'bek",
+//     familiya:"Valiyev",
+//     yoshi: 10,
+//     manzil: "Sadacha MFY",
+// }
+
+// for (let w in obj2){
+//     console.log(w, " = ", obj2[w]);
+// }
+
+// console.log("======================================");
+
+// let b = Object.assign({}, obj2)
+// b.ism = "Eshmat"
+// b.familiya = "Aliyev"
+// b.yoshi = 8
+// for (let f in b){
+//     console.log(f, " = ", b[f]);
+// }
+
+// ========================================
+
+// let obj = {
+//     ism: "Shuxrat",
+//     familiya: "Valiev",
+//     ochestvo: "Fanelovich",
+//     tugilgan_yil: 1986,
+//     manzil: "Sadacha MFY"
+// }
+
+// console.log(obj);
+
+// let obj2 = {
+//     ism: "Rayhona",
+//     familiya: "Bozorova",
+//     ochestvo: "Yolchieva",
+//     tugilgan_yil: 1987,
+//     manzil: "Sadacha MFY"
+// }
+
+// console.log(obj2);
+
+
+// let obj3 = {
+//     ism: "Otabek",
+//     familiya:"Valiyev",
+//     ochestvo: "Shuxratovich",
+//     tugilgan_yil: 2011,
+//     manzil: "Sadacha MFY",
+// }
+
+// console.log(obj3);
+
+
+// let obj4 = {
+//     ism: "Ulug'bek",
+//     familiya:"Valiyev",
+//     ochestvo: "Shuxratovich",
+//     tugilgan_yil: 2014,
+//     manzil: "Sadacha MFY",
+// }
+// console.log(obj4);
+
+
+// let obj5 = {
+//     ism: "Diyorbek",
+//     familiya:"Valiyev",
+//     ochestvo: "Shuxratovich",
+//     tugilgan_yil: 2018,
+//     manzil: "Sadacha MFY",
+// }
+
+// console.log(obj5);
+
+// let obj = {
+//     sim: "Otabek",
+//     familiya: "Valiyev",
+//     yoshi: 14
+// }
+// console.log(obj.sim);
+
+// let key = "familiya"
+// let key2 = "yoshi"
+
+// console.log(obj[key, key2]);
+
