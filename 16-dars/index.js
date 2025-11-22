b8e8a291c38ff3fd7deb4f3a17365240ed811958
shox2552-png <shox2552@gmail.com>
2025-11-22 12:10:05 +0500

    changet

---
+++ 16-dars/index.js
@@ -0,0 +1,175 @@
+// let obj1 = {
+//     name: "Eshmat"
+// }
+
+// let obj2 = {
+//     age: 20
+// }
+
+// let obj = Object.assign(obj1, obj2)
+
+// console.log(obj);
+
+// let cloneobj3 = obj
+// obj.age = 30
+// obj.name = "Toshmat"
+
+// console.log(cloneobj3);
+
+// let parent = {
+//     name: "Eshmat",
+//     age: "100",
+//     child: {
+//         name: "Toshmat",
+//         age: 60,
+//         subchild: {
+//             name: "Gulmat",
+//             age: 20
+//         }
+//     }
+// }
+
+// let {name:nom, age:w,
+//     child:{name:ism, age:yosh,
+//         subchild:{name, age}
+//     }
+// } = parent;
+
+// console.log(name,age,ism,yosh,nom,w);
+
+// let obj = {
+//     name: "Eshmat",
+//     age: 20,
+//     status: "student",
+//     addres: "Uzbekistan",
+//     ball: 90,
+// }
+
+// let sum = 0
+
+// for (let a in obj){
+//     if (typeof obj[a] === "number"){
+//     sum+= obj[a]
+//     }
+// }
+
+// console.log(sum);
+
+// shu masaladan 20ta ishlab kelish
+
+// Object assing va Clone dan 10tadan ishlab kelish
+
+// ==========================================================
+// HOMEWORK.
+
+// 1) Object.assing
+// let obj1 = {
+//     ism: "Otabek",
+//     yosh: 14
+// }
+
+// let obj2 = {
+//     addres: "Sadacha MFY"
+// }
+
+// let obj = Object.assign(obj1, obj2)
+
+// console.log(obj);
+
+// // 2)
+
+// let ojb = {
+//     name: "Malibu",
+//     color: "black"
+// }
+
+// let ojb2 = {
+//     price: "33000$"
+// }
+
+// let ob = Object.assign(ojb, ojb2)
+
+// console.log(ob);
+
+// // 3)
+// let obect = {
+//     ism: "Messi",
+//     tugilgan_yil: 1987
+// }
+
+// let obect2 = {
+//     club: "MLS Inter miami"
+// }
+
+// let obect1 = Object.assign(obect, obect2)
+
+// console.log(obect1);
+
+// // 4)
+// let Object1 = {
+//     nom: "instagram"
+// }
+
+// let Object2 = {
+//     sana: "6.10.2010"
+// }
+
+// let Object0 = Object.assign(Object1, Object2)
+
+// console.log(Object0);
+
+// // 5)
+// let o1 = {
+//     nom: "Telegram"
+// }
+
+// let o2 = {
+//     sana: "14.08.2013"
+// }
+
+// let o = Object.assign(o1, o2)
+
+// console.log(o);
+
+// console.log("==============================================");
+
+
+// // 1) Clone
+
+// let cloneobj1 = obj
+// obj.ism = "Ulug`bek"
+// obj.yosh = 11
+// obj.addres = "Sadacha MFY"
+
+// console.log(cloneobj1);
+
+// // 2)
+
+// let cloneobj = ob
+// ob.name = "Gentra"
+// ob.color = "black"
+// ob.price = "14000$"
+// console.log(cloneobj);
+
+// // 3)
+
+// let cloneobj2 = obect1
+// obect1.ism = "Ronaldo"
+// obect1.tugilgan_yil = 1985
+// obect1.club= "AL Nassr"
+
+// console.log(cloneobj2);
+
+// // 4)
+
+// let cloneobj3 = Object0
+// Object0.nom= "Facebook"
+// Object0.sana= "04.02.2004"
+// console.log(cloneobj3);
+
+// // 5)
+
+// let cloneobj4 = o
+// o.nom = "Youtube"
+// o.sana = "14.02.2005"
+// console.log(cloneobj4);
