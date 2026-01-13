a74cc1db62e2705caa159afb3e6bd933275d7ce4
shox2552-png <shox2552@gmail.com>
2026-01-13 17:44:06 +0500

    coment

--- 31-dars/index.js
+++ 31-dars/index.js
@@ -2,3 +2,15 @@
 // console.log(data.getFullYear());
 // console.log(data.getMonth()+1);
 // console.log(data.getDate());
+// console.log(data.getDay());
+// console.log(data.getHours());
+// console.log(data.getMinutes());
+// console.log(data.getSeconds());
+// console.log(data.getMilliseconds());
+// console.log(data.getTime());
+// console.log(data.toDateString());
+// console.log(data.toTimeString());
+// console.log(data.toISOString());
+// console.log(data.toUTCString());
+// console.log(data.toLocaleString());
+// console.log(data.toLocaleDateString());
