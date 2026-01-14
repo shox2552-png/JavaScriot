15dd5108d31017d8b565ba057eb818cfd471aa3d
shox2552-png <shox2552@gmail.com>
2026-01-14 17:39:08 +0500

    changet

--- 31-dars JSON/index.js
+++ 31-dars JSON/index.js
@@ -1,19 +1,19 @@
-// let date  = new Date()
-// console.log(date.getFullYear());
-// console.log(date.getMonth()+1);
-// console.log(date.getDate());
-// console.log(date.getDay());
-// console.log(date.getHours());
-// console.log(date.getMinutes());
-// console.log(date.getSeconds());
-// console.log(date.getMilliseconds());
-// console.log(date.getTime());
-// console.log(date.toDateString());
-// console.log(date.toTimeString());
-// console.log(date.toISOString());
-// console.log(date.toUTCString());
-// console.log(date.toLocaleString());
-// console.log(date.toLocaleDateString());
+let date  = new Date()
+console.log(date.getFullYear());
+console.log(date.getMonth()+1);
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getMilliseconds());
+console.log(date.getTime());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toISOString());
+console.log(date.toUTCString());
+console.log(date.toLocaleString());
+console.log(date.toLocaleDateString());
 
 
 // for (let i = 0; true; i++) {
---
+++ 32-dars Map{}/index.js
@@ -0,0 +1,11 @@
+// let map = new Map()
+// map.set("name","Ali")
+// map.set("age",20)
+ // console.log(map.get("name"));
+ // console.log(map.has("name"));
+ // console.log(map.size());
+// console.log(map);
+// map.delete("age")
+// console.log(map);
+// map.clear()
+// console.log(map);
