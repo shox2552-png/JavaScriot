55891d90014fb7b6b432844959b8ff2a17923eb1
shox2552-png <shox2552@gmail.com>
2026-01-19 21:04:21 +0500

    coment

--- 32-dars Map{}/index.js
+++ 32-dars Map{}/index.js
@@ -1,11 +1,48 @@
 // let map = new Map()
 // map.set("name","Ali")
-// map.set("age",20)
- // console.log(map.get("name"));
- // console.log(map.has("name"));
- // console.log(map.size());
+// console.log(map.set("age",20));
 // console.log(map);
+// console.log(Object.fromEntries(map));
+// console.log(map.get("name"));
+// console.log(map.has("name"));
+// console.log(map.size);
 // map.delete("age")
 // console.log(map);
 // map.clear()
 // console.log(map);
+
+// ==============================================
+
+// new Set
+
+
+// let arr1 = [1, 2, 3, 5, 6, 1, 2, 5, 3, 2, 6, 3]
+// let arr = new Set(arr1) // bir xil malumot 2chi martta qaytmaydi
+// arr.add(1) // faqat 1ta dan qiymat qoshib boladi
+// arr.add(2)
+// arr.add(3)
+// console.log(arr);
+// let newArr = []
+// arr1.forEach((n) => !newArr.includes(n) && newArr.push(n))
+// console.log(newArr);
+
+// let arr = new Set()
+// arr.add("Oxford")
+// arr.add("Algoritm")
+// arr.add("SPC")
+// arr.add("Ar-Bi")
+// arr.add("Nis")
+// console.log(arr);
+// console.log(arr.has("Ar-Bi"));
+// console.log(arr.has("SPC"));
+
+// let obj = new Map()
+
+// let arr = [
+//     ["name", "Ali"],
+//     ["id", 1],
+//     ["status", "student"],
+//     ["age", 20]
+// ]
+// arr.forEach((key, value) => obj.set(key, value))
+// console.log(obj);
