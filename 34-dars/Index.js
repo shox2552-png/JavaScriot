cbe172d4822f35202bc579066d73263d7ddfc810
shox2552-png <shox2552@gmail.com>
2026-01-24 20:23:03 +0500

    coment

---
+++ 34-dars/Index.js
@@ -0,0 +1,49 @@
+// 1.
+let str = "Men Algoritm IT markazida tahsil olaman";
+// Berilgan matndan birinchi uchragan unli harf bilan oxirgi
+// uchragan undosh harf indexlari ayirmasini chiqaring.
+
+// let str = "Men Algoritm IT markazida tahsil olaman";
+
+let vowels = "aeiou";
+
+let firstVowelIndex = -1;
+for (let i = 0; i < str.length; i++) {
+  if (vowels.includes(str[i])) {
+    firstVowelIndex = i;``
+    break;
+  }
+}
+
+let lastConsonantIndex = -1;
+for (let i = str.length - 1; i >= 0; i--) {
+  if (/[a-z]/.test(str[i]) && !vowels.includes(str[i])) {
+    lastConsonantIndex = i;
+    break;
+  }
+}
+
+
+let result = firstVowelIndex - lastConsonantIndex;
+
+console.log("Birinchi unli index:", firstVowelIndex);
+console.log("Oxirgi undosh index:", lastConsonantIndex);
+console.log("Ayirma:", result);
+
+
+// 2.
+// let str = "A1g0r1t9m8";
+// String ichidagi raqamlarni oldingi o'ringa, harflarni esa oxirgi o'ringa o'tkazing.
+
+// 3.
+// let str = "men algoritm markazida o‘qiyman";
+// matnning o'rtasidagi 1 ta kichik harfni katta xarfga almashtirib bering.
+
+// 4.
+// let str = "Algoritm IT Markazi";
+// matn dagi harflarni katta bo'lsa kichik, kichik bo'lsa kattaga almashtirib bering.
+
+// 5.
+// let str = "a3b2c1d4";
+// xar bir harfni yonidagi raqamiga qarab harflarni takrorlab, yangi Matn hosil qiling.
+// natija: "aaabbcdddd"
