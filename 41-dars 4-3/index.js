0e848bf5f5b43e8545d73458a1c531f719aa7f70
shox2552-png <shox2552@gmail.com>
2026-02-16 16:59:15 +0500

    coment

---
+++ 41-dars 4-3/index.js
@@ -0,0 +1,81 @@
+// class Car {
+//     #password = "12345"
+//     constructor(model) {
+//         this.model = model // public
+//     }
+
+//     getPasswordData(){
+//         //private 
+//         return this.#getPassword()
+//     }
+
+//     #getPassword(){
+//         return this.#password
+//     }
+// }
+
+// let obj = new Car("GM")
+// console.log(obj.model);
+// console.log(obj.getPasswordData());
+// console.log(obj.getPassword());// notog`ri
+// console.log(obj.password); // notog`ri
+
+// Mixim
+
+
+// const Parent = {
+//     getParentName() {
+//         return "Parent"
+//     }
+// }
+
+// const Child = {
+//     getChildName(){
+//         return "Child"
+//     }
+// }
+
+// class SubChild {
+//     getSubChildName(){
+//         return "Sub child"
+//     }
+// }
+
+// Object.assign(SubChild.prototype, Parent, Child)
+
+// let obj = new SubChild()
+
+// console.log(obj.getParentName());
+
+
+const Parent = (Data) =>
+    class extends Data {
+        getParentName() {
+            return "Parent"
+        }
+    }
+
+const Parent2 = (Data) =>
+    class extends Data {
+        getParent2Name() {
+            return "Parent2"
+        }
+    }
+
+const Child = (Data) =>
+    class extends Data {
+        getChild() {
+            return "Child"
+        }
+}
+
+class Boglovchi{}
+class SubChild extends Parent(Parent2(Child(Boglovchi))){
+    getSubChild(){
+        return "Sub Child"
+    }
+}
+
+let obj = new SubChild()
+console.log(obj.getParentName);
+console.log(obj.getParent2Name);
